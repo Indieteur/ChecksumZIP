5be6120af946dd4edf02edd1fffdd7f040609a99
Indieteur <drannoyingonion@gmail.com>
2019-04-24 22:46:02 +0800

    Add project files.

---
+++ ChecksumZIP/ChecksumZIP.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.IO.Compression;
+
+namespace Indieteur.ChecksumZIP
+{
+    public class FileAlreadyExistsException : Exception
+    {
+        public FileAlreadyExistsException (string message) : base(message)
+        {
+
+        }
+    }
+
+    public class FailedChecksumException : Exception
+    {
+        public FailedChecksumException(string message) : base(message)
+        {
+
+        }
+    }
+
+    public class NoChecksumStoredException : Exception
+    {
+        public NoChecksumStoredException(string message) : base(message)
+        {
+
+        }
+    }
+    public static class ChecksumZIP
+    {
+
+        public static void CreateArchiveFromDirectory(string sourceDirectory, string destinationFile, bool storeChecksumInArchive = true, bool overwrite = true, CompressionLevel compressionLevel = CompressionLevel.Optimal, bool includeBaseDirectory = false, int checksumBufferSize = ChecksumZIPHelper.KB_4_IN_BYTES)
+        {
+            CreateArchiveFromDirectory(sourceDirectory, destinationFile, storeChecksumInArchive, overwrite, compressionLevel, includeBaseDirectory, Encoding.ASCII, checksumBufferSize);
+        }
+
+        public static void CreateArchiveFromDirectory(string sourceDirectory, string destinationFile, bool storeChecksumInArchive, bool overwrite, CompressionLevel compressionLevel, bool includeBaseDirectory, Encoding encoding, int checksumBufferSize = ChecksumZIPHelper.KB_4_IN_BYTES)
+        {
+            if (!Directory.Exists(sourceDirectory))
+                throw new DirectoryNotFoundException(sourceDirectory + " was not found.");
+            if (File.Exists(destinationFile))
+            {
+                if (overwrite)
+                    File.Delete(destinationFile);
+                else
+                    throw new FileAlreadyExistsException(destinationFile + " already exists.");
+            }
+            ZipFile.CreateFromDirectory(sourceDirectory, destinationFile, compressionLevel, includeBaseDirectory, encoding);
+            if (storeChecksumInArchive)
+                AppendChecksumToArchive(destinationFile, checksumBufferSize);
+        }
+
+        public static void AppendChecksumToArchive (string archivePath, int checksumBufferSize = ChecksumZIPHelper.KB_4_IN_BYTES)
+        {
+            if (!File.Exists(archivePath))
+                throw new FileNotFoundException(archivePath + " was not found.");
+            string stringToAppend = ChecksumZIPHelper.CalculateMD5(archivePath, checksumBufferSize) + ChecksumZIPHelper.MAGIC_CONSTANT;
+            byte[] byteToAppend = Encoding.ASCII.GetBytes(stringToAppend);
+            ChecksumZIPHelper.AppendAllBytes(archivePath, byteToAppend);
+        }
+
+        public static string RemoveChecksumValueFromArchive (string archivePath)
+        {
+            if (!File.Exists(archivePath))
+                throw new FileNotFoundException(archivePath + " was not found.");
+            if (!ArchiveHasChecksumValue(archivePath))
+                return null;
+            byte[] byteStore = new byte[ChecksumZIPHelper.MD5_HASH_BYTE_LENGTH + ChecksumZIPHelper.MAGIC_CONSTANT.Length];
+            using (var stream = new FileStream(archivePath, FileMode.Open))
+            {
+                stream.Seek(stream.Length - byteStore.Length, SeekOrigin.Begin);
+                stream.Read(byteStore, 0, byteStore.Length);
+                stream.Seek(0, SeekOrigin.Begin);
+                stream.SetLength(stream.Length - (ChecksumZIPHelper.MD5_HASH_BYTE_LENGTH + ChecksumZIPHelper.MAGIC_CONSTANT.Length));
+            }
+            string byteToString = Encoding.ASCII.GetString(byteStore);
+            return byteToString.Substring(0, byteToString.Length - ChecksumZIPHelper.MAGIC_CONSTANT.Length);
+        }
+
+        public static bool ArchiveHasChecksumValue(string archivePath)
+        {
+            if (!File.Exists(archivePath))
+                throw new FileNotFoundException(archivePath + " was not found.");
+            byte[] byteStore = new byte[ChecksumZIPHelper.MAGIC_CONSTANT.Length];
+            ChecksumZIPHelper.ReadEndBytes(archivePath, byteStore);
+            return Encoding.ASCII.GetString(byteStore) == ChecksumZIPHelper.MAGIC_CONSTANT;
+        }
+
+        public static string ReadArchiveStoredChecksumValue(string archivePath)
+        {
+            if (!File.Exists(archivePath))
+                throw new FileNotFoundException(archivePath + " was not found.");
+            if (!ArchiveHasChecksumValue(archivePath))
+                throw new NoChecksumStoredException(archivePath + " doesn't have a stored checksum value.");
+
+            byte[] byteStore = new byte[ChecksumZIPHelper.MD5_HASH_BYTE_LENGTH + ChecksumZIPHelper.MAGIC_CONSTANT.Length];
+            ChecksumZIPHelper.ReadEndBytes(archivePath, byteStore);
+            string byteToString = Encoding.ASCII.GetString(byteStore);
+            return byteToString.Substring(0, byteToString.Length - ChecksumZIPHelper.MAGIC_CONSTANT.Length);
+        }
+
+        public static string CalculateChecksumOfArchive(string archivePath, int checksumBufferSize = ChecksumZIPHelper.KB_4_IN_BYTES)
+        {
+            if (!File.Exists(archivePath))
+                throw new FileNotFoundException(archivePath + " was not found.");
+            return ChecksumZIPHelper.CalculateMD5(archivePath, checksumBufferSize);
+        }
+
+        public static bool ArchiveHasMatchingStoredChecksum(string archivePath, int checksumBufferSize = ChecksumZIPHelper.KB_4_IN_BYTES)
+        {
+            return (ReadArchiveStoredChecksumValue(archivePath) == CalculateChecksumOfArchive(archivePath, checksumBufferSize));
+        }
+
+        public static void ExtractZip(string archivePath, string destination, bool performSpecialChecksumCheck = true, int checksumBufferSize = ChecksumZIPHelper.KB_4_IN_BYTES)
+        {
+            if (!File.Exists(archivePath))
+                throw new FileNotFoundException(archivePath + " was not found.");
+            if (!Directory.Exists(destination))
+                throw new DirectoryNotFoundException(destination + " destination directory could not be found.");
+            if (performSpecialChecksumCheck)
+            {
+                if (ArchiveHasMatchingStoredChecksum(archivePath, checksumBufferSize) == false)
+                    throw new FailedChecksumException("Calculated checksum for file " + archivePath + " does not match with the one stored in it.");
+            }
+            ZipFile.ExtractToDirectory(archivePath, destination);
+            
+        }
+    }
+}
---
+++ ChecksumZIP/Helper.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indieteur.ChecksumZIP
+{
+    public static class ChecksumZIPHelper
+    {
+        public const int MD5_HASH_BYTE_LENGTH = 32;
+        public const int MB_10_IN_BYTES = 10485760;
+        public const int KB_4_IN_BYTES = 4096;
+        public const float GB_10_TO_MB_10_RATIO = 0.0005f;
+        public const string MAGIC_CONSTANT = "ChecksumZIP";
+
+
+
+
+        public static int GetSuggestedBufferSize(string pathToFile, int min = KB_4_IN_BYTES, int max = MB_10_IN_BYTES, float ratio = GB_10_TO_MB_10_RATIO)
+        {
+            if (!File.Exists(pathToFile))
+                throw new FileNotFoundException(pathToFile + " was not found.");
+            if (min < 1)
+                throw new ArgumentException("Minimum buffer size cannot be less than 1 byte.");
+            if (max < 1)
+                throw new ArgumentException("Maximum buffer size cannot be less than 1 byte.");
+
+            FileInfo fInfo = new FileInfo(pathToFile);
+            return IntClamp((int)(fInfo.Length * ratio), min, max);
+
+
+        }
+
+        public static int IntClamp(int value, int min, int max)
+        {
+            if (value < min)
+                return min;
+            else if (value > max)
+                return max;
+            else
+                return value;
+        }
+        public static string CalculateMD5(string filename, int bufferSize = 4096) // From - https://stackoverflow.com/questions/10520048/calculate-md5-checksum-for-a-file
+        {
+            if (bufferSize < 1)
+                throw new ArgumentException("Buffer size cannot be less than 1 byte.");
+            string hashString = null;
+           
+            using (var md5 = MD5.Create())
+            {
+                using (var stream = new BufferedStream(File.Open(filename, FileMode.Open), bufferSize))
+                {
+                    bool md5HashAtEnd = false;
+                    int totalLengthOfChecksumZIPStoredHash = MAGIC_CONSTANT.Length + MD5_HASH_BYTE_LENGTH;
+                    if (stream.Length > totalLengthOfChecksumZIPStoredHash)
+                    {
+                        stream.Seek(stream.Length - MAGIC_CONSTANT.Length, SeekOrigin.Begin);
+                        byte[] byteToRead = new byte[MAGIC_CONSTANT.Length];
+                        stream.Read(byteToRead, 0, byteToRead.Length);
+                        if (Encoding.ASCII.GetString(byteToRead) == MAGIC_CONSTANT)
+                            md5HashAtEnd = true;
+                        else
+                            goto noMAGICCONSTANT;
+                        stream.SetLength(stream.Length - totalLengthOfChecksumZIPStoredHash);
+                    }
+                    noMAGICCONSTANT:
+                    stream.Seek(0, SeekOrigin.Begin);
+                    var hash = md5.ComputeHash(stream);
+                    hashString = BitConverter.ToString(hash).Replace("-", "").ToLowerInvariant();
+                    if (md5HashAtEnd)
+                    {
+                        byte[] byteArray = Encoding.ASCII.GetBytes(hashString + MAGIC_CONSTANT);
+                        stream.Seek(stream.Length, SeekOrigin.Begin);
+                        stream.Write(byteArray, 0, byteArray.Length);
+                    }
+                }
+            }
+            return hashString;
+        }
+
+      
+
+        public static string CalculateMD5(byte[] array)
+        {
+            using (var md5 = MD5.Create())
+            {
+                var hash = md5.ComputeHash(array);
+                return BitConverter.ToString(hash).Replace("-", "").ToLowerInvariant();
+            }
+        }
+
+        public static void AppendAllBytes(string path, byte[] bytes) // From - https://stackoverflow.com/questions/6862368/c-sharp-append-byte-array-to-existing-file
+        {
+            //argument-checking here.
+
+            using (var stream = new FileStream(path, FileMode.Append))
+            {
+                stream.Write(bytes, 0, bytes.Length);
+            }
+        }
+
+        public static void ReadEndBytes(string path, byte[] bytes)
+        {
+            using (BinaryReader reader = new BinaryReader(new FileStream(path, FileMode.Open)))
+            {
+                reader.BaseStream.Seek(reader.BaseStream.Length - bytes.Length, SeekOrigin.Begin);
+                reader.Read(bytes, 0, bytes.Length);
+            }
+        }
+    }
+}
